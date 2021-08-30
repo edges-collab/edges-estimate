a919c1c051734a726b6f8d636e9fcb59b8e31f58
Steven Murray <steven.murray@curtin.edu.au>
2021-08-30 10:32:13 -0700

    chore: remove stuff from setup.py

--- setup.py
+++ setup.py
@@ -2,63 +2,6 @@
 
 """The setup script."""
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
-with open("README.rst") as readme_file:
-    readme = readme_file.read()
-
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
-
-requirements = [
-    "Click>=6.0",
-    "cached_property",
-    "numpy",
-    "scipy",
-    "yabf @ https://github.com/steven-murray/yabf",
-]
-
-setup_requirements = [
-    "pytest-runner",
-]
-
-test_requirements = [
-    "pytest",
-]
-
-setup(
-    author="Steven Murray",
-    author_email="steven.g.murray@asu.edu",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-    ],
-    description="Constrain parameters with EDGES data and an Emulator",
-    entry_points={
-        "console_scripts": [
-            "edges_estimate=edges_estimate.cli:main",
-        ],
-    },
-    install_requires=requirements,
-    license="GNU General Public License v3",
-    long_description=readme + "\n\n" + history,
-    include_package_data=True,
-    keywords="edges_estimate",
-    name="edges_estimate",
-    packages=find_packages(include=["edges_estimate"]),
-    setup_requires=setup_requirements,
-    test_suite="tests",
-    tests_require=test_requirements,
-    url="https://github.com/steven-murray/edges_estimate",
-    use_scm_version=True,
-    zip_safe=False,
-)
+setup(use_scm_version=True)
