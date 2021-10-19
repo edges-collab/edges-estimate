236b73ac77f95b1298a73f269d80207886ec1b3b
nmahesh1412 <nivedita.mahesh@asu.edu>
2021-10-18 23:42:42 -0700

    working test for the plot function

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -1,17 +1,21 @@
 
 from edges_estimate import plots as p
-from getdist import loadMCSamples
+import numpy as np
 import pytest
 
-@pytest.fixture(scope="function")
-def test_mcsamples(mcsamples):
-    samples= loadMCSamples('sample')
-    return samples
-
-def test_model_from_mc_samples(samples):
-    models= p.get_models_from_mcsamples(samples, lk_names = ["lowband"])
-    assert len(models) ==1
+@pytest.fixture(scope="session")
+def models():
+    models = {key: {'model': np.random.normal(size=(20, 50))} for key in ['one', 'two']}
     return models
 
-def test_def_make_residual_plot_shaded(models):
-    p.make_residual_plot_shaded()
+@pytest.fixture(scope="session")
+def freqs():
+    return  {key:  np.random.normal(size=50) for key in ['one', 'two']}
+    
+@pytest.fixture(scope="session")
+def temp():
+    return  {key: np.random.normal(size=50) for key in ['one', 'two']}
+
+def test_def_make_residual_plot_shaded(models,freqs,temp):
+    p.make_residual_plot_shaded(models,freqs,temp)
+
