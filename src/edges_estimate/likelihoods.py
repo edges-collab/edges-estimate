fixed the needed float precision for radiometric white noise

src/edges_estimate/likelihoods.py
@@ -71,7 +71,7 @@ class RadiometricAndWhiteNoise(MultiComponentChi2):
         Parameter("sigma_wn", 0.0, min=0, latex=r"\sigma_{\rm wn}"),
     ]
 
-    integration_time = attr.ib(converter=np.float, kw_only=True)  # in seconds!
+    integration_time = attr.ib(converter=np.float64, kw_only=True)  # in seconds!
     weights = attr.ib(1, kw_only=True)
 
     @weights.validator